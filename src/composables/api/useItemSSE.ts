50a8b55c6c1cfe5e381cce74b2e2f3775bc1c4e7
Swunci <Swunci@gmail.com>
2026-05-22 20:51:48 -0400

    refactor: moved event handlers out of useItemSEE
    To prevent duplicate event handlers from being registered when calling useItemSSE in different places

--- src/composables/api/useItemSSE.ts
+++ src/composables/api/useItemSSE.ts
@@ -2,19 +2,16 @@ import { endpoints } from '@/api/endpoints'
 import type { ItemEvent } from '@/types/item'
 import { ref } from 'vue'
 
-const eventSource = ref<EventSource | null>(null)
 const itemEvent = ref<ItemEvent | null>(null)
 
+const eventSource = new EventSource(endpoints.items.getItemEvents)
+eventSource.onopen = () => console.log('SSE connected')
+eventSource.onerror = (e) => console.log('SSE error', e)
+eventSource.onmessage = (event) => {
+  itemEvent.value = JSON.parse(event.data)
+  console.log(`Item event recieved, feed_id ${itemEvent.value?.feedId}`)
+}
+
 export function useItemSSE() {
-  console.log('useItemSSE called, eventSource:', eventSource.value)
-  if (!eventSource.value) {
-    eventSource.value = new EventSource(endpoints.items.getItemEvents)
-    eventSource.value.onopen = () => console.log('SSE connected')
-    eventSource.value.onerror = (e) => console.log('SSE error', e)
-    eventSource.value.onmessage = (event) => {
-      console.log('Item event recieved')
-      itemEvent.value = JSON.parse(event.data)
-    }
-  }
   return { itemEvent }
 }
